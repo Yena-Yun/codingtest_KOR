4eca2795b91fe3f9431edd8dcda05ba3bdd7b512
Yena-Yun <cheryleduart@gmail.com>
2021-10-07 11:48:12 +0900

    Home 및 body 최상단 css

--- src/Pages/BeerList.js
+++ src/Pages/BeerList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import beerdata from '../data.json';
 import MaterialTable, { MTableToolbar, MTableActions } from 'material-table';
 // import { NumberOutlined } from '@ant-design/icons';
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,64 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
   const history = useHistory();
 
   return (
-    <>
-      <div>홈페이지입니다.</div>
-      <button
+    <Wrapper>
+      <Title>Tradir BeerShop!</Title>
+      <MoveBtn
         onClick={() => {
           history.push('/beerlist');
         }}
       >
-        BeerList로 이동하기
-      </button>
-    </>
+        move to
+        <Strong>BeerList</Strong>
+      </MoveBtn>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 160px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  font-family: 'Carter One', cursive;
+  letter-spacing: 1px;
+  color: #292929;
+`;
+
+const MoveBtn = styled.button`
+  width: 300px;
+  padding: 12px 0;
+  border-radius: 30px;
+  font-size: 28px;
+  background: #8af7ae;
+  color: #2c2c2c;
+  border: none;
+  cursor: pointer;
+  font-family: 'Nanum Pen Script', cursive;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+
+  &:hover {
+    background: #96fbc4;
+    color: #000;
+    font-size: 28.5px;
+    transform: translateY(1px);
+    transition: all 250ms ease-in-out;
+  }
+`;
+
+const Strong = styled.span`
+  font-weight: 700;
+  margin-left: 10px;
+`;
 
 export default Home;
